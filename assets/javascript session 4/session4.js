7b15c8f2f5f4d8bcc9c39344889fd0bb19a4a76f
sujal <sujalshinde635@gmail.com>
2024-10-14 19:26:43 +0530

    task update

---
+++ assets/javascript session 4/session4.js
@@ -0,0 +1,37 @@
+// "use strict";
+x=89;
+console.log(x);
+
+
+
+let var1=900;
+
+try 
+{
+adddlert("Welcome guest!");
+}
+catch(err) 
+{
+console.log(err.message);
+}
+
+function test()
+{
+    let a=90;
+}
+
+hello = () => {
+    return "Hello World!";
+  }
+
+  let myFunction = (a, b) => a * b;
+
+// Create an object:
+const person = {
+    firstName: "ABC",
+    lastName: "PQR",
+    id: 5566,
+    fullName : function() {
+    return this.firstName + " " + this.lastName;
+    }
+};
